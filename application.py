234bdf48815a85e37550dccf1e667eb2b86a3592
karianjahi <kitgaedesigaidi8@gmail.com>
2023-08-29 11:15:33 +0200

    rendering the recommendations template with jinja tags and carrying the movies variable to the html file

--- application.py
+++ application.py
@@ -19,16 +19,8 @@ def index():
 
 @app.route("/recommender")
 def recommend_movies():
-    movies = ["movie1", "movie2", "movie3"]
-    return f'''
-            <h1>These are your movie recommendations</h1>
-            <ol>
-                <li>{movies[0]}</li>
-                <li>{movies[1]}</li>
-                <li>{movies[2]}</li>
-            </ol>
-             '''
-
+    random_movies = ["movie1", "movie2", "movie3"]
+    return render_template("recommendations.html", movies=random_movies)
 # To test if the server can open, we shall make sure that only when this file is used directly is when we can open a new web server
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
